4f368b94b5619d38b6764f72a54af1f0f66adae1
Анна Шахбазян <fatamorgana9205@gmail.com>
2018-08-31 12:53:26 +0300

    init

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
-import MyComponent from "../../lib";
+import axios from 'axios'
+import Carousel from "../../lib";
 import "./styles.css";
 
-function Demo() {
-  return (
-    <div>
-      <h1>Demo with examples of the component</h1>
-      <MyComponent color="brown">Wow what a button</MyComponent>
-    </div>
-  );
+const CLIENT_ID = '31cf57d170cbbbbc9c5f5f671cb612b275d5b3a83722ecebd754bf4661fe0388'
+const server = 'https://api.unsplash.com'
+
+class Demo extends Component {
+  state = {
+    photos: []
+  }
+
+  componentWillMount () {
+    axios.get(`${server}/search/photos/?client_id=${CLIENT_ID}&per_page=10&query=animal&orientation=landscape`)
+      .then((res) => {
+        console.log(res)
+        this.setState({photos: res.data.results})
+      })
+  }
+
+  renderImages = () => {
+    const { photos } = this.state
+    if (!photos.length) return <div>Loading...</div>
+    return photos.map((img) => <img key={img.id} src={img.urls.small} alt={"photo"} /> )
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 style={{textAlign: 'center', margin: '40px 0'}}>Simple react 3d carousel</h1>
+        <Carousel>
+          {this.renderImages()}
+        </Carousel>
+      </div>
+    )
+  }
+
 }
 
 render(<Demo />, document.getElementById("app"));
--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,18 +1,101 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
+import './index.css'
 
-class MySuperCoolComponent extends Component {
-  handleClick = () => {
-    console.log("Click!");
-  };
+class Carousel extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      classes: []
+    }
+  }
 
-  render() {
-    const { color, children } = this.props;
+  static getDerivedStateFromProps (props, state) {
+    if (state.classes.length) return null
+
+    const classes = createClassesArray(props)
+    return {classes}
+
+    function createClassesArray (props) {
+      const { children } = props
+      if (!Array.isArray(children)) return []
+
+      let id =  Math.floor((children.length - 1) / 2)
+      let direction = 'before'
+      let classesArray = []
+
+      children.forEach(() => {
+        if (id === 0) direction = 'current'
+        if (id < 0) direction = 'after'
+
+        classesArray.push({classname: direction, id: Math.abs(id)})
+        id--
+      })
+
+      return classesArray
+    }
+  }
+
+
+
+  moveSlide = (direction, count) => {
+    console.log('click')
+    let { classes } = this.state
+
+    if (direction === 'after') {
+      let start = classes.length - count
+      let last = classes.splice(start,count)
+      classes = [...last, ...classes]
+    } else {
+      let first = classes.splice(0, count)
+      classes = [...classes, ...first]
+    }
+    this.setState({classes})
+    console.log(classes)
+
+  }
+
+  sliderRef = (node) => {this.slider = node}
+
+  renderSlides = () => {
+    const { children } = this.props
+    if (!Array.isArray(children)) return <div></div>
+
+    const { classes } = this.state
+
+    return children.map((img, index) => {
+      let classNames = 'slider3d__item slider3d__item--' + classes[index].classname + ' slider3d__item--' + classes[index].id
+      let direction = classes[index].classname
+
+      return (
+        <li key={index} className={classNames} onClick={() => this.moveSlide(direction, classes[index].id)} >
+          {img}
+          {/*
+            classes[index].id < 6 && classes[index].id > -6 ?
+              <img src={img} alt={'slide ' + index} style={{display: 'block'}} />
+              :
+              <img data-src={img} alt={'slide ' + index} style={{display: 'block'}} />
+          */}
+        </li>
+      )}
+    )
+  }
+
+  render () {
+
     return (
-      <button onClick={this.handleClick} style={{ color }}>
-        {children}
-      </button>
-    );
+      <div className="slider3d">
+        <ul className="slider3d__items">
+          {this.renderSlides()}
+        </ul>
+
+        <nav>
+          <button className="slider3d__button slider3d__button--left" onClick={() => this.moveSlide('before', 1)}></button>
+          <button className="slider3d__button slider3d__button--right" onClick={() => this.moveSlide('after', 1)}></button>
+        </nav>
+      </div>
+    )
   }
 }
 
-export default MySuperCoolComponent;
+
+export default Carousel
